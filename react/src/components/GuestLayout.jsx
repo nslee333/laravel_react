abd2270afdb94a16b8db58cf5f0373ea1987c637
Nathan <nslee333@gmail.com>
2023-05-17 21:15:24 -0700

    Added outlet for children elements.

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+
 export default function GuestLayout(){
   return (
-    <>
-      GuestLayout
-    </>
+    <div>
+      For guest users only
+      <Outlet />
+    </div>
   )
 }
