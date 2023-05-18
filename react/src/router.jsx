d268c31cc5ba349a0d2061af581065c165d2e9a2
Nathan <nslee333@gmail.com>
2023-05-17 20:54:34 -0700

    Added component routes.

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,7 +1,21 @@
 import {createBrowserRouter} from "react-router-dom";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Users from "./views/Users";
 
 const router = createBrowserRouter([
-  
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+  {
+    path: '/users',
+    element: <Users />
+  },
 ])
 
 export default router;
