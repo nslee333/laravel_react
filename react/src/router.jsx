c344e1b085c4eb54eade44d059d6e68c6e0d9fb0
Nathan <nslee333@gmail.com>
2023-05-17 21:09:14 -0700

    Changed routing - and moved view components into children routes.

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -3,21 +3,41 @@ import Login from "./views/Login";
 import Signup from "./views/Signup";
 import Users from "./views/Users";
 import NotFound from "./views/NotFound";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Dashboard from "./views/Dashboard";
 
 
 const router = createBrowserRouter([
   {
-    path: '/login',
-    element: <Login />
+    path: '/',
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      },
+      {
+        path: '/users',
+        element: <Users />
+      },
+    ]
   },
   {
-    path: '/signup',
-    element: <Signup />
+    path: '/',
+    element: <GuestLayout />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/signup',
+        element: <Signup />
+      },
+    ]
   },
-  {
-    path: '/users',
-    element: <Users />
-  },
+  
   {
     path: '*',
     element: <NotFound />
