f8d9d49a3611269a134cbb97a05d5c69201b5aee
Nathan <nslee333@gmail.com>
2023-05-18 18:01:38 -0700

    Added structure and function skele

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom"
+
 export default function Signup() {
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
+
   return (
-    <div>
-      Signup
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">
+            Signup for free
+          </h1>
+          <input type="email" placeholder="Full Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password Confirmation" />
+          <button className="btn btn-block">Signup</button>
+          <p className="message">
+            Already Registered? <Link to="/login">Sign In</Link>
+          </p>
+        </form>
+      </div>
     </div>
   )
 }
