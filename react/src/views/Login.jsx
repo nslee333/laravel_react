1a47cbccc1476cba04b37a744920e4d3d783ba8e
Nathan <nslee333@gmail.com>
2023-05-18 17:55:01 -0700

    Added structure and function skeleton

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,7 +1,22 @@
 export default function Login() {
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
+
   return (
-    <div>
-      Login
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button className="btn btn-block">Login</button>
+          <p className="message">
+            Not Registered? <Link to="/signup">Create an account</Link>
+          </p>
+        </form>
+      </div>
     </div>
   )
 }
