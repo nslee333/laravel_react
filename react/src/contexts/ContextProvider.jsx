4a130c628d3594c0b6172ce5a08e338cc70f6f18
Nathan <nslee333@gmail.com>
2023-05-19 12:16:33 -0700

    Reverted token state hook to original from temporary development setup.

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -11,10 +11,7 @@ export const ContextProvider = ({ children }) => {
   const [user, setUser] = useState({
     name: "john"
   });
-  // const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
-  // const [token, _setToken] = useState(123);
-  const [token, _setToken] = useState(null);
-
+  const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
 
   const setToken = (token) => {
     _setToken(token);
