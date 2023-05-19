7761897a39f2f7489e4db41dfb85b2d27eb0e757
Nathan <nslee333@gmail.com>
2023-05-18 17:50:25 -0700

    Added username display and logout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -7,6 +7,10 @@ export default function DefaultLayout(){
   if (!token) {
     return <Navigate to="/login" />
   }
+
+  const onLogout = (e) => {
+    e.preventDefault();
+  }
   
   return (
     <div id="defaultLayout">
@@ -20,7 +24,8 @@ export default function DefaultLayout(){
             Header
           </div>
           <div>
-            User info
+            {user.name}
+            <a href="#" onClick={onLogout} className="btn-logout">Logout</a>
           </div>
         </header>
         <main>
