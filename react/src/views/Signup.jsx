ded4e52d05ffeefc984653816b3fef4f80d392e1
Nathan <nslee333@gmail.com>
2023-05-18 18:35:35 -0700

    Added refs for input forms, filled onSubmit with payload and axios req

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,9 +1,40 @@
 import { Link } from "react-router-dom"
+import { useRef } from "react";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Signup() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+
+
+  const {setUser, setToken} = useStateContext();
+
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value
+    }
+
+    axiosClient.post('/signup', payload)
+      .then(({data}) => {
+        setUser(data.user);
+        setToken(data.token);
+      })
+      .catch(err => {
+        const response = err.response;
+
+        if (response && response.status === 422) {
+          console.log(response.data.errors);
+        }
+      })
   }
 
 
@@ -13,10 +44,10 @@ export default function Signup() {
         <h1 className="title">
           Signup for free
         </h1>
-        <input type="email" placeholder="Full Name" />
-        <input type="email" placeholder="Email Address" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Password Confirmation" />
+        <input ref={nameRef} type="text" placeholder="Full Name" />
+        <input ref={emailRef} type="email" placeholder="Email Address" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <input ref={passwordConfirmationRef} type="password" placeholder="Password Confirmation" />
         <button className="btn btn-block">Signup</button>
         <p className="message">
           Already Registered? <Link to="/login">Sign In</Link>
