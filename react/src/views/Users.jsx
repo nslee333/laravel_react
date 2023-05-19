08828ed347519fa1850f6ebf799038fb7ac15471
Nathan <nslee333@gmail.com>
2023-05-19 15:16:50 -0700

    Filled in Users display

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axiosClient from "../axios-client";
+import { Link, useFetchers } from "react-router-dom";
 
 
 export default function Users() {
@@ -10,13 +11,24 @@ export default function Users() {
     getUsers();
   }, [])
 
+  const onDelete = (user) => {
+    if (!window.confirm("Are you sure you want to delete this user?")) {
+      return
+    }
+
+    axiosClient.delete(`${import.meta.env.VITE_API_BASE_URL}/api/users/${user.id}`)
+    .then(() => {
+      getUsers();
+    })
+  }
+
   const getUsers = () => {
     setLoading(true);
 
     axiosClient.get(`${import.meta.env.VITE_API_BASE_URL}/api/users`)
       .then(({data}) => {
         setLoading(false);
-        console.log(data);
+        setUsers(data.data);
       })
       .catch(() => {
         setLoading(false);
@@ -25,8 +37,49 @@ export default function Users() {
 
 
   return (
-    <div>
-
+    <div className="">
+      <div style={{display:'flex', justifyContent: "space-between", alignItems: "center"}}>
+        <h1>Users</h1>
+        <Link to="/users/new" className="btn-add">Add new</Link>
+      </div>
+    <div className="Card animated fadeInDown">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Create Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        {loading && 
+        <tbody>
+          <tr>
+            <td colSpan="5" className="text-center">
+              Loading...
+            </td>
+          </tr>
+        </tbody>
+        }
+       {!loading && <tbody>
+          {users.map(u => (
+            <tr key={u.id}>
+              <td>{u.id}</td>
+              <td>{u.name}</td>
+              <td>{u.email}</td>
+              <td>{u.password}</td>
+              <td>{u.created_at}</td>
+              <td>
+                <Link to={'/users/' + u.id} className='btn-edit'>Edit</Link>
+                &nbsp;
+                <button onClick={e => onDelete(u)}className='btn-delete'>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>}
+      </table>
+    </div>
     </div>
   )
 }
