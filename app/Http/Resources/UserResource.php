96a72963ea1d6a7b28bd1e0b872cf00bfacc3640
Nathan <nslee333@gmail.com>
2023-05-19 13:26:25 -0700

    Added field names.

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,12 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+        ];
+
     }
 }
