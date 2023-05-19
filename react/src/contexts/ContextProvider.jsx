7a6f783bcb5f9ef60b1fc8787199dfb9ef27b9fe
Nathan <nslee333@gmail.com>
2023-05-18 17:58:16 -0700

    Added useState for dev purposes

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -12,7 +12,8 @@ export const ContextProvider = ({ children }) => {
     name: "john"
   });
   // const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
-  const [token, _setToken] = useState(123);
+  // const [token, _setToken] = useState(123);
+  const [token, _setToken] = useState(null);
 
 
   const setToken = (token) => {
